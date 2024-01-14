a9419e66c72751cb2209e262debf1e2493ba27c8
Thiago Meneghetti Rossi <thiago.meneghetti@clubedodev.com.br>
2024-01-13 22:52:46 -0300

    botoes selecionar e cancelar

--- custom.js
+++ custom.js
@@ -1,11 +1,14 @@
-var  dataHoraFinalString = "2024-01-19 00:00:00"; 
+// var  dataHoraFinalString = "2024-01-19 00:00:00"; 
 
 const dataInput = document.getElementById('data');
-
+const btnSelecionar = document.getElementById('selecionar_data');
+const btnCancelar = document.getElementById('cancelar_data');
 // dataInput.addEventListener("change", (event) => {
-//     var  dataHoraFinalString = dataInput.value.replace('T', '-');
-    
-//     console.log(dataHoraFinalString);
+btnSelecionar.addEventListener('click', (event) =>{
+    var  dataHoraFinalString = dataInput.value.replace('T', '-');
+    btnSelecionar.disabled = true
+    btnCancelar.disabled = false
+    // console.log('dataHoraFinalString',dataHoraFinalString);
     
     
     const partesDataHora  = dataHoraFinalString.split(/[\s:-]+/)
@@ -13,7 +16,7 @@ const dataInput = document.getElementById('data');
 
     const dataHoraFinal = new Date(partesDataHora[0], partesDataHora[1] -1, partesDataHora[2],
     partesDataHora[3], partesDataHora[4], partesDataHora[5] || 0)
-    console.log(partesDataHora[1] -1);
+    // console.log(partesDataHora[1] -1);
     // console.log(dataHoraFinal);
 
     const interValo = setInterval(() => {
@@ -26,14 +29,16 @@ const dataInput = document.getElementById('data');
         // console.log(diferenca);
 
         if (diferenca <= 0) {
-            clearInterval(interValo);
-
-            document.getElementById("dias").innerHTML = 0;
-            document.getElementById("horas").innerHTML = 0;
-            document.getElementById("minutos").innerHTML = 0;
-            document.getElementById("segundos").innerHTML = 0;
+            resetarContador();
+            btnSelecionar.disabled = false;
             return false;
         }
+        btnCancelar.addEventListener('click', (event) =>{
+            resetarContador();
+            btnSelecionar.disabled = false
+            return false;
+        });
+
 
         const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
         const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -46,4 +51,13 @@ const dataInput = document.getElementById('data');
         document.getElementById("segundos").innerHTML = `${segundos}s`;
 
     }, 1000);
-// });
+
+    function resetarContador(){
+        clearInterval(interValo);
+        document.getElementById("dias").innerHTML = 0;
+        document.getElementById("horas").innerHTML = 0;
+        document.getElementById("minutos").innerHTML = 0;
+        document.getElementById("segundos").innerHTML = 0;
+    }
+});
+
